40d6b8a170a567442ee47a849085596beb8e3498
arequipa2000 <arequipa2000@gmail.com>
2024-04-27 15:38:38 -0500

    iniciando

---
+++ src/com/aluracursos/radioalura/modelos/Audio.java
@@ -0,0 +1,55 @@
+package com.aluracursos.radioalura.modelos;
+
+public class Audio {
+    private String titulo;
+    private int duracion;
+    private int totalDeReproducciones;
+    private int meGusta;
+    private int totalDeMeGusta;
+    public void meGusta(){
+        this.totalDeMeGusta++;
+    }
+    public  void reproduce(){
+        this.totalDeReproducciones++;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public int getTotalDeReproducciones() {
+        return totalDeReproducciones;
+    }
+
+    public void setTotalDeReproducciones(int totalDeReproducciones) {
+        this.totalDeReproducciones = totalDeReproducciones;
+    }
+
+    public int getMeGusta() {
+        return meGusta;
+    }
+
+    public void setMeGusta(int meGusta) {
+        this.meGusta = meGusta;
+    }
+
+    public int getTotalDeMeGusta() {
+        return totalDeMeGusta;
+    }
+
+    public void setTotalDeMeGusta(int totalDeMeGusta) {
+        this.totalDeMeGusta = totalDeMeGusta;
+    }
+}
